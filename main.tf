e45d74384fe6c1c51cd3699a0ec6d45bda177cba
smt197 <105449441+smt197@users.noreply.github.com>
2026-06-11 21:03:17 +0000

    fix1

--- main.tf
+++ main.tf
@@ -21,16 +21,14 @@ resource "aws_instance" "ubuntu_server" {
               systemctl enable docker
               systemctl start docker
 
-              usermod -aG docker root
-
-              ln -s /usr/bin/docker /usr/local/bin/docker || true
+              usermod -aG docker ubuntu
 
                 # Vérification
               docker --version
               docker compose version
                 # création d'un répertoire pour l'application
-              mkdir -p /home/root/fitness-221
-              chown root:root /home/root/fitness-221
+              mkdir -p /home/ubuntu/fitness-221
+              chown ubuntu:ubuntu /home/ubuntu/fitness-221
               EOF
 
   tags = {
